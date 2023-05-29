5843b2fa9e7ab4cb0a31d652538a77a9d6b50448
Luigi Quattrociocchi <luigi@quattrociocchi.net>
2023-05-28 21:32:25 -0400

    update build scripts

--- make.bat
+++ make.bat
@@ -10,8 +10,9 @@ if %ERRORLEVEL% NEQ 0 (echo ERROR: "cl" not found - please run this from the MSV
 
 set GLFW=lib/glfw-3.3.8.bin.WIN64
 set IMGUI=external/imgui
+set IMGUI_CLUB=external/imgui_club
 
-set INCLUDES=/I%IMGUI% /I%IMGUI%/backends /I%GLFW%/include
+set INCLUDES=/Iexternal /I%IMGUI% /I%IMGUI%/backends /I%GLFW%/include /I%IMGUI_CLUB%/imgui_memory_editor
 set SOURCES=src/*.cpp %IMGUI%/backends/imgui_impl_glfw.cpp %IMGUI%/backends/imgui_impl_opengl3.cpp %IMGUI%/imgui*.cpp
 set LIBS=%GLFW%/lib-vc2022/glfw3.lib opengl32.lib gdi32.lib shell32.lib
 
--- make.sh
+++ make.sh
@@ -3,12 +3,13 @@
 # Install glfw from package manager
 # sudo apt install libglfw3
 IMGUI="external/imgui"
+IMGUI_CLUB="external/imgui_club"
 
-INCLUDES="-I$IMGUI -I$IMGUI/backends"
+INCLUDES="-Iexternal -I$IMGUI -I$IMGUI/backends -I$IMGUI_CLUB/imgui_memory_editor"
 SOURCES="src/*.cpp $IMGUI/backends/imgui_impl_glfw.cpp $IMGUI/backends/imgui_impl_opengl3.cpp $IMGUI/imgui*.cpp"
 LIBS="-lglfw -lrt -lm -ldl -lGL"
 
-CFLAGS="-std=c++20 -g -Wall -Wextra -Wshadow -Wswitch-enum -Wpedantic -Werror -O2"
+CFLAGS="-std=c++20 -g -Wall -Wextra -Wshadow -Wswitch-enum -Wpedantic -O2"
 
 set -xe
 
